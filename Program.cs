2c4da5381239205ae7f3af88f204b76715eff653
hahahalol <davilacerdalf@gmail.com>
2015-11-15 16:16:19 -0200

    New Hahaha's Tahm Kench
    My first script

--- Program.cs
+++ Program.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LeagueSharp;
+﻿using LeagueSharp;
 using LeagueSharp.Common;
+using System;
 
-
-namespace Tahm_Kench
+namespace Hahaha_s_Tahm_Kench
 {
     class Program
     {
-        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
-
-        private static Orbwalking.Orbwalker Orbwalker;
-
-        private static Spell Q, W, E, R;
-
-        private static Menu Menu;
-        
         static void Main(string[] args)
         {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
 
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.CharData.BaseSkinName != "Tahm Kench")
+            {
+                return;
+            }
+
+            Tahm_Kench.OnLoad();
+            Game.PrintChat("Welcome to Hahaha's Tahm Kench!");
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tahm Kench")]
+[assembly: AssemblyTitle("Hahaha's Tahm Kench")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tahm Kench")]
+[assembly: AssemblyProduct("Hahaha's Tahm Kench")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bf0a76d3-fab9-4abd-95c6-ca507c693dac")]
+[assembly: Guid("71338c5d-f0c3-4371-b9c5-582b168d3996")]
 
 // Version information for an assembly consists of the following four values:
 //
